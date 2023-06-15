9adf333a2ef75fc4892c24261f16b0ce966509de
Juan LB <jlacayo43235@ufide.ac.cr>
2023-06-15 14:24:31 -0600

    Inicio de Proyec

--- Documents/NetBeansProjects/ProyectoFinal/src/main/java/com/mycompany/proyectofinal/ProyectoFinal.java
+++ Documents/NetBeansProjects/ProyectoFinal/src/main/java/com/mycompany/proyectofinal/ProyectoFinal.java
@@ -13,5 +13,6 @@ public class ProyectoFinal {
     public static void main(String[] args) {
         System.out.println("Hola Mundo 3!");
         System.out.println("Esto es una prueba");
+         System.out.println("Buscando la solución");
     }
 }
