829b09e16329d23ef94c023209e06fb71d43be1e
Juan LB <jlacayo43235@ufide.ac.cr>
2023-06-15 13:40:54 -0600

    Inicio de Proyecto U

--- Documents/NetBeansProjects/ProyectoFinal/src/main/java/com/mycompany/proyectofinal/ProyectoFinal.java
+++ Documents/NetBeansProjects/ProyectoFinal/src/main/java/com/mycompany/proyectofinal/ProyectoFinal.java
@@ -1,6 +1,5 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
 package com.mycompany.proyectofinal;
@@ -12,6 +11,6 @@ package com.mycompany.proyectofinal;
 public class ProyectoFinal {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
+        System.out.println("Hola Mundo 3!");
     }
 }
