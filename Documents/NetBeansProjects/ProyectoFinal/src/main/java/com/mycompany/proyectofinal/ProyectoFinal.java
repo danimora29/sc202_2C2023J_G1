ed28983d8a1088c69f0f467ecb8fe1f58cfb7ed8
Juan LB <jlacayo43235@ufide.ac.cr>
2023-06-15 14:00:14 -0600

    Inicio de Proyec

--- Documents/NetBeansProjects/ProyectoFinal/src/main/java/com/mycompany/proyectofinal/ProyectoFinal.java
+++ Documents/NetBeansProjects/ProyectoFinal/src/main/java/com/mycompany/proyectofinal/ProyectoFinal.java
@@ -12,5 +12,6 @@ public class ProyectoFinal {
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo 3!");
+        System.out.println("Esto es una prueba");
     }
 }
