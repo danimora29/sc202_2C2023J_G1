623ee36fa1c32f59c1d02b6f779e8dce1cc85023
Juan LB <jlacayo43235@ufide.ac.cr>
2023-06-16 15:47:55 -0600

    Inicio de Proyec

---
+++ Documents/NetBeansProjects/ProyectoBarberos/src/main/java/com/mycompany/proyectobarberos/ProyectoBarberos.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectobarberos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ProyectoBarberos {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo 1");
+          System.out.println("Hola Mundo 2");
+            System.out.println("Hola Mundo 3");
+              System.out.println("Hola Mundo 4");
+    }
+}
