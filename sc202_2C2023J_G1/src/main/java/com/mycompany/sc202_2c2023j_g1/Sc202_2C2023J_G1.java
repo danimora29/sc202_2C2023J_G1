2d7005d2c65dc3f8d9c268823455e85bdc8ac370
Juan LB <jlacayo43235@ufide.ac.cr>
2023-06-16 18:31:45 -0600

    Prueba P

--- sc202_2C2023J_G1/src/main/java/com/mycompany/sc202_2c2023j_g1/Sc202_2C2023J_G1.java
+++ sc202_2C2023J_G1/src/main/java/com/mycompany/sc202_2c2023j_g1/Sc202_2C2023J_G1.java
@@ -11,6 +11,9 @@ package com.mycompany.sc202_2c2023j_g1;
 public class Sc202_2C2023J_G1 {
 
     public static void main(String[] args) {
-        System.out.println("Hola esto es una prueba proyecto Barbería");
+        System.out.println("Hola esto es una prueba proyecto Barbería 1");
+        System.out.println("Hola esto es una prueba proyecto Barbería 2");
+        System.out.println("Hola esto es una prueba proyecto Barbería 3");
+        System.out.println("Hola esto es una prueba proyecto Barbería 4");
     }
 }
