ad8bd07d0c3e959dcbd33c953dfd57ebb5b8d980
Juan LB <jlacayo43235@ufide.ac.cr>
2023-06-16 19:23:51 -0600

    BarberShop Proyec

---
+++ Documents/NetBeansProjects/BarberShop/src/main/java/com/mycompany/barbershop/BarberShop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.barbershop;
+
+/**
+ *
+ * @author Usuario
+ */
+public class BarberShop {
+
+    public static void main(String[] args) {
+        System.out.println("Hola estoy probando proyecto BarberShop!");
+    }
+}
