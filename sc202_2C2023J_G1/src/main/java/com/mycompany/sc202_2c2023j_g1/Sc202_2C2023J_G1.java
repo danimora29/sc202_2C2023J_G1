e07248221665a284de3316932eb396dc61592344
Juan LB <jlacayo43235@ufide.ac.cr>
2023-06-16 18:20:35 -0600

    Prueba Proyec

---
+++ sc202_2C2023J_G1/src/main/java/com/mycompany/sc202_2c2023j_g1/Sc202_2C2023J_G1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sc202_2c2023j_g1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Sc202_2C2023J_G1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola esto es una prueba proyecto Barbería");
+    }
+}
