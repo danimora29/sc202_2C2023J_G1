aa92cfa55fb4205940ce0995873f9f0a81e3605e
Juan LB <jlacayo43235@ufide.ac.cr>
2023-06-16 19:38:31 -0600

    BarberShop Pro

---
+++ sc202_2C2023J_G1/src/main/java/com/mycompany/sc202_2c2023j_g1/Barbero.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sc202_2c2023j_g1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Barbero {
+    public String Nombre;
+    public int TiempoAlmuerzo;
+    public int Ganancias;
+
+    
+}
